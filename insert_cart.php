0ccab9f89361135c090cfb9d2a34b128f68ba824
Isaac Martens <isaac.martens@uleth.ca>
2021-11-30 19:21:35 -0700

    initial cart pages

---
+++ insert_cart.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+<h1> Insert Cart</h1>
+
+
+
+</head>
+
+<body>
+<form name="form" action="insert.php" method="POST">
+<input name="cart_ID" title="fill in your cart_ID" type="text" id="cart_ID" placeholder="ID"><br><br>
+<input name="item_QTY" title="fill the quantity of items" type="number" id="item_QTY" placeholder="Quantity"><br><br>
+<input name="cost" title="fill in the item cost" type="number" id="cost" placeholder="Cost"><br><br>
+<input name="purchase_ID" title="fill in the purchase ID" type="text" id="purchase_ID" placeholder="Purchase ID"><br><br>
+<input name="pro_ID" title="fill in the product ID" type="text" id="pro_ID" placeholder="Product ID"><br><br>
+<input name="submit" type="submit" id="submit" value="Submit"><br><br>
+
+
+
+</form>
+</body>
+</html>
---
+++ select_cart.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<style>
+
+th {
+text-align: left;
+padding: 8px;
+background-color: #04AA6D;
+color: white;
+}
+</head>
+</style>
+
+<body>
+<table boarder = 4px; width=100%;>
+
+<th>Cart ID</th>
+<th>Quantity</th>
+<th>Cost</th>
+<th>Purchase ID</th>
+<th>Product ID</th>
+
+</body>
+</html>
