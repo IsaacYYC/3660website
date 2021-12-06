fcf40011c69fa51d9ae04864b17bda794dacd2b8
JPLachowskiUleth <95326375+JPLachowskiUleth@users.noreply.github.com>
2021-12-06 07:56:09 -0700

    Update index.php

--- Products/index.php
+++ Products/index.php
@@ -1,1 +1,25 @@
-me(Jacob) I will add my files here soon
+<html lang="en">
+<head>
+
+<h1> Welcome to Our Online Store </h1>
+
+</head>
+
+<?php
+include('dbconnection.php');
+?>
+
+<body>
+
+<form name="form" action="insert.php" method="POST">
+<input name="Product name" title="fill in Product Name" type="text" id="pro_name" placeholder="name"><hr>
+<input name="Product ID" title="fill in Product ID" type="text" id="proID" placeholder="proID"<hr><br<br>
+<input name="Product Description" title="fill in Product Description" type="text" id="pro_desc" placeholder="pro_desc"<hr><br<br>
+<input name="Product Price" title="fill in Product Price" type="text" id="pro_price" placeholder="pro_price"<hr><br<br>
+<input name="submit" type="submit" id="submit" value="Submit"><hr>
+
+</form>
+
+<a href="select.php">Select Query</a>
+</body>
+</html>
